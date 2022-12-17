758b75ae33099263b55241822a2cb634c8c563ec
Vadim <59086091+1Valerion1@users.noreply.github.com>
2022-12-17 20:01:54 +0700

    Initial commit

---
+++ Models/Actions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_laba_back_1.Models
+{
+    public class Actions
+    {
+        public int val1 { get; set; }
+        public int val2 { get; set; }
+        public int div { get; set; }
+        public int sub { get; set; }
+        public int add { get; set; }
+        public int mul { get; set; }
+    }
+}
